b4b3d588d1c2c102e14bb1d19d4e9a8dfc1b5fa7
maestroanth <maestroanth@gmail.com>
2017-05-16 17:52:56 -0600

    remember update with git clone https://github.com/angular/angular-cli.git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { BannerInlineComponent } from './banner-inline/banner-inline.component';
+import { BannerComponent } from './banner-inline/banner-inline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerInlineComponent
+    BannerComponent
   ],
   imports: [
     BrowserModule,
